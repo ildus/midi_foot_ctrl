d447e8b893a3f1a5856306134b3b80bf47957aa4
Ildus Kurbangaliev <i.kurbangaliev@gmail.com>
2019-12-26 01:24:13 +0500

    add forgotten file, cosmetic changes

---
+++ main/captive.c
@@ -0,0 +1,119 @@
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+
+#include <lwip/netdb.h>
+
+static bool dns_enabled = true;
+static const char * TAG = "dns";
+
+static int create_udp_socket(int port)
+{
+    struct sockaddr_in saddr = {0};
+    int sock = -1;
+    int err = 0;
+
+    sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_IP);
+    if (sock < 0)
+    {
+        ESP_LOGE(TAG, "Failed to create socket. Error %d", errno);
+        return -1;
+    }
+
+    // Bind the socket to any address
+    saddr.sin_family = PF_INET;
+    saddr.sin_port = htons(port);
+    saddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    err = bind(sock, (struct sockaddr *)&saddr, sizeof(struct sockaddr_in));
+    if (err < 0)
+    {
+        ESP_LOGE(TAG, "Failed to bind socket. Error %d", errno);
+        goto err;
+    }
+
+    // All set, socket is configured for sending and receiving
+    return sock;
+
+err:
+    close(sock);
+    return -1;
+}
+
+static void dns_main(void * pvParameters)
+{
+    uint8_t data[128];
+    int len = 0;
+    struct sockaddr_in client = {0};
+    socklen_t client_len = sizeof(struct sockaddr_in);
+    uint32_t i = 0;
+
+    ESP_LOGI(TAG, "DNS server start ...");
+
+    int sock = create_udp_socket(53);
+
+    if (sock < 0)
+        ESP_LOGE(TAG, "Failed to create IPv4 multicast socket");
+
+    while (dns_enabled)
+    {
+        len = recvfrom(sock, data, 100, 0, (struct sockaddr *)&client, &client_len);
+
+        if ((len < 0) || (len > 100))
+        {
+            ESP_LOGE(TAG, "recvfrom error\n");
+            continue;
+        }
+
+        printf("DNS request:");
+        for (i = 0x4; i < len; i++)
+        {
+            if ((data[i] >= 'a' && data[i] <= 'z') || (data[i] >= 'A' && data[i] <= 'Z') || (data[i] >= '0' && data[i] <= '9'))
+                printf("%c", data[i]);
+            else
+                printf("_");
+        }
+        printf("\r\n");
+
+        data[2] |= 0x80;
+        data[3] |= 0x80;
+        data[7] = 1;
+
+        data[len++] = 0xc0;
+        data[len++] = 0x0c;
+
+        data[len++] = 0x00;
+        data[len++] = 0x01;
+        data[len++] = 0x00;
+        data[len++] = 0x01;
+
+        data[len++] = 0x00;
+        data[len++] = 0x00;
+        data[len++] = 0x00;
+        data[len++] = 0x0A;
+
+        data[len++] = 0x00;
+        data[len++] = 0x04;
+
+        data[len++] = 192;
+        data[len++] = 168;
+        data[len++] = 4;
+        data[len++] = 1;
+
+        sendto(sock, data, len, 0, (struct sockaddr *)&client, client_len);
+        vTaskDelay(10);
+    }
+
+    ESP_LOGI(TAG, "DNS server stopped");
+    shutdown(sock, 0);
+    close(sock);
+    vTaskDelete(NULL);
+}
+
+void shutdown_dns()
+{
+    dns_enabled = false;
+}
+
+void start_dns_server()
+{
+    xTaskCreate(&dns_main, "dns_task", 2048, NULL, 5, NULL);
+}
--- main/components.js
+++ main/components.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6 */
 let tmpl = document.querySelector('#x-action-select-template');
 let initialMap = {};
 
@@ -6,70 +7,70 @@ if (initial_values)
 {
     let lines = initial_values.split('\n');
     lines.forEach(line => {
-        if (line.length == 0)
+        if (line.length === 0)
             return;
 
         let parts = line.split(':');
         let id = parts[0];
-        let vals = parts[1].split('&')
+        let vals = parts[1].split('&');
 
         initialMap[id] = [];
-		let curval = {};
+        let curval = {};
         vals.forEach(val => {
             let keyval = val.split('=');
             curval[keyval[0]] = keyval[1];
-			if (keyval[0] == "d2")
-			{
-				initialMap[id].push(curval);
-				curval = {};
-			}
-        })
+            if (keyval[0] == "d2")
+            {
+                initialMap[id].push(curval);
+                curval = {};
+            }
+        });
     });
 }
 
 function setup_inputs(parent_el, callback, vals)
 {
-	let action_select = parent_el.querySelector('select[name="action"]');
-	action_select.addEventListener("change", callback);
+    let action_select = parent_el.querySelector('select[name="action"]');
+    action_select.addEventListener("change", callback);
 
-	let ch_select = parent_el.querySelector('select[name="channel"]');
-	ch_select.addEventListener("change", callback);
+    let ch_select = parent_el.querySelector('select[name="channel"]');
+    ch_select.addEventListener("change", callback);
 
-	let input1 = parent_el.querySelector('input[name="d1"]');
-	input1.addEventListener("change", callback);
+    let input1 = parent_el.querySelector('input[name="d1"]');
+    input1.addEventListener("change", callback);
 
-	let input2 = parent_el.querySelector('input[name="d2"]');
-	input2.addEventListener("change", callback);
+    let input2 = parent_el.querySelector('input[name="d2"]');
+    input2.addEventListener("change", callback);
 
-	if (vals !== undefined)
-	{
-        action_select.value = vals["action"];
-        ch_select.value = vals["channel"];
-        input1.setAttribute("value", vals["d1"]);
-        input2.setAttribute("value", vals["d2"]);
-	}
+    if (vals !== undefined)
+    {
+        action_select.value = vals.action;
+        ch_select.value = vals.channel;
+        input1.setAttribute("value", vals.d1);
+        input2.setAttribute("value", vals.d2);
+    }
 }
 
 function extend_form(form, callback, vals)
 {
-	let fields = form.querySelector(".fields");
-	let cloned = fields.cloneNode(true);
-	let inner_add = cloned.querySelector('button.add');
-	inner_add.innerHTML = '&minus;';
+    let fields = form.querySelector(".fields");
+    let cloned = fields.cloneNode(true);
+    let inner_add = cloned.querySelector('button.add');
+    inner_add.innerHTML = '&minus;';
 
-	let inner_play = cloned.querySelector('button.play');
-	inner_play.setAttribute('disabled', true);
+    let inner_play = cloned.querySelector('button.play');
+    inner_play.setAttribute('disabled', true);
 
-	cloned.querySelector('button.add').addEventListener('click', function (e) {
-		e.preventDefault();
-		cloned.remove();
-		callback();
-	});
+    cloned.querySelector('button.add').addEventListener('click', function (e) {
+        e.preventDefault();
+        cloned.remove();
+        callback();
+    });
 
-	setup_inputs(cloned, callback, vals);
-	form.appendChild(cloned);
-	callback();
-};
+    setup_inputs(cloned, callback, vals);
+    form.appendChild(cloned);
+    callback();
+}
 
 customElements.define('action-select', class extends HTMLElement {
     constructor() {
@@ -107,15 +108,15 @@ customElements.define('action-select', class extends HTMLElement {
         update_form_values();
 
         let vals = initialMap[this.getAttribute("id")];
-		setup_inputs(form, update_form_values, vals[0]);
+        setup_inputs(form, update_form_values, vals[0]);
 
-		if (vals.length > 1)
-		{
-			for (let i=1; i < vals.length; i++)
-				extend_form(form, update_form_values, vals[i]);
-		}
+        if (vals.length > 1)
+        {
+            for (let i=1; i < vals.length; i++)
+                extend_form(form, update_form_values, vals[i]);
+        }
 
-		update_form_values();
+        update_form_values();
         form.querySelector("button.play").addEventListener("click", function (e) {
             e.preventDefault();
 
@@ -126,9 +127,9 @@ customElements.define('action-select', class extends HTMLElement {
                 if (res.ok)
                     show_notify("Played");
                 else
-					res.text().then(text => {
-						show_notify("Error: " + text, true);
-					});
+                    res.text().then(text => {
+                        show_notify("Error: " + text, true);
+                    });
             }).catch(res => {
                 show_notify("Error: " + res, true);
             });
@@ -136,33 +137,33 @@ customElements.define('action-select', class extends HTMLElement {
 
         form.querySelector("button.add").addEventListener("click", function (e) {
             e.preventDefault();
-			extend_form(form, update_form_values);
+            extend_form(form, update_form_values);
         });
     }
 });
 
 function show_notify(msg, error) {
-	let notify = document.getElementById("notify");
-	let notify_class = notify.classList;
+    let notify = document.getElementById("notify");
+    let notify_class = notify.classList;
 
-	if (error)
-		notify_class.add("alert-danger");
-	else
-		notify_class.add("alert-primary");
+    if (error)
+        notify_class.add("alert-danger");
+    else
+        notify_class.add("alert-primary");
 
-	notify.innerHTML = msg;
-	notify.style.display = "inline";
+    notify.innerHTML = msg;
+    notify.style.display = "inline";
 
-	setTimeout(function () {
-		notify.innerHTML = "";
-		notify_class.remove("alert-danger");
-		notify_class.remove("alert-primary");
-		notify.style.display = "none";
-	}, 2000);
+    setTimeout(function () {
+        notify.innerHTML = "";
+        notify_class.remove("alert-danger");
+        notify_class.remove("alert-primary");
+        notify.style.display = "none";
+    }, 2000);
 }
 
 function show_error(msg) {
-	show_notify(msg, true);
+    show_notify(msg, true);
 }
 
 window.onload = function () {
@@ -180,17 +181,17 @@ window.onload = function () {
         }
 
         // save configuration
-		console.log(value);
+        console.log(value);
         fetch("/configure", {
             method: "POST",
             body: value
         }).then(res => {
             if (res.ok)
-			    show_notify("saved");
+                show_notify("saved");
             else
-				res.text().then(text => {
-					show_notify("Error: " + text, true);
-				});
+                res.text().then(text => {
+                    show_notify("Error: " + text, true);
+                });
 
             main_btn.removeAttribute("disabled");
         }).catch(res => {
